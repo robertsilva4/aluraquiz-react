8dd2ad249c6826adaf0a97884e3fb5b667cd8eb9
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-26 11:34:05 -0300

    code finished in the first class

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import styled from "styled-components";
 import db from "../db.json";
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
-
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,62 +23,28 @@ export const QuizContainer = styled.div`
   }
 `;
 
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4caf50;
-  background-color: #1c1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1,
-  h2,
-  h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`; 
-
-Widget.Content = styled.div`
-  padding: 24px 32px 32px 32px;
-  & > *:first-child {
-    margin-top: 0;
-  }
-
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-`;
 
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg} projectUrl="https://github.com/robertsilva4">
       <QuizContainer>
         <Widget>
-          <Widget.Content>
+          <Widget.Header>
             <h1>the Legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
             <p>Lorem Ipsum</p>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
-            <h1>Quiz da Gaalera</h1>
+            <h1>Quizes da Galera</h1>
             <p>Lorem Ipsum</p>
           </Widget.Content>
         </Widget>
+        <Footer/>
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/robertsilva4"/>
+    </QuizBackground>
   );
 }
