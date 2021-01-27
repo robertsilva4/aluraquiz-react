66b2d85ec93739f0678486db4686943b3b9d2583
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-27 00:42:12 -0300

    adding page quiz

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import styled from "styled-components";
-import db from "../db.json";
+import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import styled from 'styled-components';
+import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -23,17 +29,34 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
   return (
     <QuizBackground backgroundImage={db.bg} projectUrl="https://github.com/robertsilva4">
+      <Head>
+        <title>Alura Quiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>the Legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem Ipsum</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              const name = 'Robert';
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do React');
+
+              // router envia para próxima página
+            }}
+            >
+              <input placeholder="Preencha com seu nome" />
+              <button type="submit">
+                Jogar [seuNome]
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -42,9 +65,9 @@ export default function Home() {
             <p>Lorem Ipsum</p>
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/robertsilva4"/>
+      <GitHubCorner projectUrl="https://github.com/robertsilva4" />
     </QuizBackground>
   );
 }
